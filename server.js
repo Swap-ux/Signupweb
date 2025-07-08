597014e9f04883037a4161636c8af18b9ff5b5d2
Swap-ux <swapnil_ug_24@ei.nits.ac.in>
2025-07-08 14:22:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get('/api/dashboard', authenticate, (req, res) => {
 
 
 const PORT = process.env.PORT || 3000;
-const HOST = '0.0.0.0'; // 👈 REQUIRED on Render
+const HOST = '0.0.0.0'; 
 
 app.listen(PORT, HOST, () => {
   console.log(`🚀 Server running at http://${HOST}:${PORT}`);
